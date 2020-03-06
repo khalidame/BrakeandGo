878a3029faca336e9ab30fd99df4e313fbe677a3
unknown <dalidausa@gmail.com>
2020-03-05 22:09:45 -0500

    My First Commit

---
+++ includes/header.php
@@ -0,0 +1,51 @@
+
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  <link rel="stylesheet" type="text/css" href="Styles/Stylesheet.css" />
+
+</head>
+
+
+
+
+  <div class="container-fluid">
+
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Login|Sign up</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Your Cart</a>
+      </li>
+
+    </ul>
+
+
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
+      <a class="navbar-brand" href="#">Logo</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link mr-5" href="#">Parts <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mr-5" href="#">Tools</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mr-5" href="#">Oil & Fluids</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link mr-5" href="#">Contact us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
