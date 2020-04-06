1e7501ab64737dfca5c856c02edef8f20d69abd0
unknown <brakeandgoap@gmail.com>
2020-04-06 17:44:57 -0400

    header/sign up-in restyling

--- login.php
+++ login.php
@@ -1,24 +1,30 @@
-
-<?php require_once 'includes/header.php'?>
+<?php require_once 'includes/header.php' ?>
 
 
 <div class="container-fluid">
   <div class="row justify-content-center">
     <div class="col-sm-12 col-md-8">
-    
-<form action="login-submit.php" method="post" class="text-center border border-light px-3 py-3">
 
-    <p class="h4 mb-4">Sign in</p>
+      <form action="login-submit.php" method="post" class="text-center border border-light px-3 pt-3">
 
-    <input type="text" id="username" name="username"  class="form-control mb-4" placeholder="username">
+        <p class="h4 mb-4">Sign in</p>
 
-    <input type="password" id="password" name="password" class="form-control mb-4" placeholder="Password">
-  
-    <button class="btn btn-primary my-4 btn-block" type="submit">Sign in</button>
-    <p>If you don't have an account, please click below</p>
-   
-</form>
-<button class="btn border border-color btn-block my-4" type="submit"><a href="registration.php">Sign up</a></button>
-</div>
-</div>
-</div>
+        <input type="text" id="username" name="username" class="form-control mb-4" placeholder="username*">
+
+        <input type="password" id="password" name="password" class="form-control mb-4" placeholder="Password*">
+
+        <button class="btn btn-primary my-4 btn-block" type="submit">Sign in</button>
+        <p>If you don't have an account, please <a href="registration.php"> click here</a> to register</p>
+      </form>
+      
+      <div class="row justify-content-center px-3">
+        <div class="col text-center">
+          <p>If you forgot your username/password please reset below </p>
+        </div>
+        <div>
+          <button class="btn border border-color mb-3 mr-3">Forgot Username</button>
+          <button class="btn border border-color mb-3">Forget Password</button>
+        </div>
+      </div>
+    </div>
+  </div>
--- registration.php
+++ registration.php
@@ -1,14 +1,11 @@
 <?php require_once 'includes/header.php' ?>
-<?php require_once 'includes/functions.php' ?>
 
 
 <div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col-sm-12 col-md-8">
-            <form action="reg-submit.php" method="post" class="text-center border border-light px-3 py-3">
-
+            <form action="reg-submit.php" method="post" class="text-center border border-light px-3 pt-3">
                 <p class="h4 mb-4">Sign up</p>
-
                 <div class="form-row mb-4">
                     <div class="col">
                         <input type="text" id="First_name" name="First_name" class="form-control" placeholder="First name*">
@@ -17,11 +14,9 @@
                         <input type="text" id="Last_name" name="Last_name" class="form-control" placeholder="Last name*">
                     </div>
                 </div>
-
                 <input type="email" id="Email" name="Email" class="form-control mb-4" placeholder="E-mail*">
 
                 <!-- <input type="tel" id="Phone" name='phone'  class="form-control mb-4" placeholder="Phone number*"> -->
-
                 <input type="text" id="username" name="username" class="form-control mb-4" placeholder="Username*">
 
                 <div class="form-row mb-4">
@@ -34,8 +29,12 @@
                 </div>
                 <button class="btn btn-primary btn-block my-4" type="submit">Sign up</button>
             </form>
-            <button class="btn btn-block border border-color my-4">Forgot Username</button>
-            <button class="btn btn-block border border-color my-4">Forget Password</button>
+
+            <div class="row justify-content-center px-3">
+                <div class="col text-center">
+                    <p>If you are already registered please <a href="login.php"> click here</a> to sign in </p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
